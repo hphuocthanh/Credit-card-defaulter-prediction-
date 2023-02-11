05130b32b1e9bb2552551db7af26c87f08d2356e
Thanh Phuoc <phuocthanhqt113@gmail.com>
2023-02-11 21:35:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ S_file = open('scaler.pkl','rb')
 scaler = pickle.load(S_file)
 
 # creating 4 Dicts which is used to convert to str to int befor giveing input to ML module
-S_DICT = {'Male':1,'Female':2}
-M_DICT = {'Married':1,'Single':2,'Others':3}
-E_DICT ={'Graduate school':1,'University':2,'High school':3,'Others':4}
-PAY_DICT= {'Zero Bill':0,'Paid duly':-1,'1 Month Delay':1,'2 Months Delay':2,'3 Months Delay':3,'4 Months Delay':4,'5 Months Delay':5,'6 Months Delay':6,'7 Months Delay':7,'8 Months Delay':8,'9 Months & Above Delay':9}
+# S_DICT = {'Male':1,'Female':2}
+# M_DICT = {'Married':1,'Single':2,'Others':3}
+# E_DICT ={'Graduate school':1,'University':2,'High school':3,'Others':4}
+# PAY_DICT= {'Zero Bill':0,'Paid duly':-1,'1 Month Delay':1,'2 Months Delay':2,'3 Months Delay':3,'4 Months Delay':4,'5 Months Delay':5,'6 Months Delay':6,'7 Months Delay':7,'8 Months Delay':8,'9 Months & Above Delay':9}
 
 # while loop for Dynamic Months
 n=0
@@ -54,15 +54,15 @@ def multi_cust(file):
     cust_ID_DF['customer_id/Name'] = df['Customer_ID/Name']
     df.drop('Customer_ID/Name',axis=1,inplace=True)
     df['AVG_BILL_AMT'] = ((df['1st Month.1']+df['2nd Month.1']+df['3rd Month.1']+df['4th Month.1']+df['5th Month.1']+df['6th Month.1'])/6)
-    df['SEX'] = df['SEX'].apply(lambda x: S_DICT[x])
-    df['EDUCATION'] = df['EDUCATION'].apply(lambda x: E_DICT[x])
-    df['MARRIAGE'] = df['MARRIAGE'].apply(lambda x: M_DICT[x])
-    df['1st Month'] = df['1st Month'].apply(lambda x: PAY_DICT[x])
-    df['2nd Month'] = df['2nd Month'].apply(lambda x: PAY_DICT[x])
-    df['3rd Month'] = df['3rd Month'].apply(lambda x: PAY_DICT[x])
-    df['4th Month'] = df['4th Month'].apply(lambda x: PAY_DICT[x])
-    df['5th Month'] = df['5th Month'].apply(lambda x: PAY_DICT[x])
-    df['6th Month'] = df['6th Month'].apply(lambda x: PAY_DICT[x])
+#     df['SEX'] = df['SEX'].apply(lambda x: S_DICT[x])
+#     df['EDUCATION'] = df['EDUCATION'].apply(lambda x: E_DICT[x])
+#     df['MARRIAGE'] = df['MARRIAGE'].apply(lambda x: M_DICT[x])
+#     df['1st Month'] = df['1st Month'].apply(lambda x: PAY_DICT[x])
+#     df['2nd Month'] = df['2nd Month'].apply(lambda x: PAY_DICT[x])
+#     df['3rd Month'] = df['3rd Month'].apply(lambda x: PAY_DICT[x])
+#     df['4th Month'] = df['4th Month'].apply(lambda x: PAY_DICT[x])
+#     df['5th Month'] = df['5th Month'].apply(lambda x: PAY_DICT[x])
+#     df['6th Month'] = df['6th Month'].apply(lambda x: PAY_DICT[x])
     # Scaling the model
     df_scaled = scaler.transform(df)
     # Pridecting
@@ -96,9 +96,9 @@ def main():
     form = sl.form('Customer Details')
     C_name = form.text_input('Name')
     col1,col2,col3 = form.columns(3)
-    C_sex= S_DICT[col1.radio('Sex',options=['Male','Female'])]
-    C_marg = M_DICT[col2.radio('Marital status',options=['Married','Single','Others'])]
-    C_edu = E_DICT[col3.radio('Education',options=['Graduate school','University','High school','Others'])]
+#     C_sex= S_DICT[col1.radio('Sex',options=['Male','Female'])]
+#     C_marg = M_DICT[col2.radio('Marital status',options=['Married','Single','Others'])]
+#     C_edu = E_DICT[col3.radio('Education',options=['Graduate school','University','High school','Others'])]
     col1,col2 = form.columns(2)
     C_age = col1.number_input('Age')
     C_Limit = col2.number_input('Limit Amout in $')
@@ -106,14 +106,14 @@ def main():
     
     L_opt = ['Zero Bill','Paid duly','1 Month Delay','2 Months Delay','3 Months Delay','4 Months Delay','5 Months Delay','6 Months Delay','7 Months Delay','8 Months Delay','9 Months & Above Delay']
     form.subheader('Repayment Status for last 6 months')
-    col1,col2,col3=form.columns(3)
-    pay1=PAY_DICT[col1.selectbox(label=Dynamic_months['m5'],options=L_opt,key=1)]
-    pay2=PAY_DICT[col2.selectbox(label=Dynamic_months['m4'],options=L_opt,key=2)]
-    pay3=PAY_DICT[col3.selectbox(label=Dynamic_months['m3'],options=L_opt,key=3)]
-    col4,col5,col6=form.columns(3)
-    pay4=PAY_DICT[col4.selectbox(label=Dynamic_months['m2'],options=L_opt,key=4)]
-    pay5=PAY_DICT[col5.selectbox(label=Dynamic_months['m1'],options=L_opt,key=5)]
-    pay6=PAY_DICT[col6.selectbox(label=Dynamic_months['m0'],options=L_opt,key=6)]
+#     col1,col2,col3=form.columns(3)
+#     pay1=PAY_DICT[col1.selectbox(label=Dynamic_months['m5'],options=L_opt,key=1)]
+#     pay2=PAY_DICT[col2.selectbox(label=Dynamic_months['m4'],options=L_opt,key=2)]
+#     pay3=PAY_DICT[col3.selectbox(label=Dynamic_months['m3'],options=L_opt,key=3)]
+#     col4,col5,col6=form.columns(3)
+#     pay4=PAY_DICT[col4.selectbox(label=Dynamic_months['m2'],options=L_opt,key=4)]
+#     pay5=PAY_DICT[col5.selectbox(label=Dynamic_months['m1'],options=L_opt,key=5)]
+#     pay6=PAY_DICT[col6.selectbox(label=Dynamic_months['m0'],options=L_opt,key=6)]
     
     form.subheader('Bill Amount in the respective months in $')
     col14,col15,col16=form.columns(3)
@@ -137,7 +137,8 @@ def main():
     
     # Creating new feature Average Bill Amount 
     avg_bill_amt = np.mean(BAmt1+BAmt2+BAmt3+BAmt4+BAmt5+BAmt6)
-    features=[C_Limit,C_sex,C_edu,C_marg,C_age,pay1,pay2,pay3,pay4,pay5,pay6,BAmt1,BAmt2,BAmt3,BAmt4,BAmt5,BAmt6,PAmt1,PAmt2,PAmt3,PAmt4,PAmt5,PAmt6,avg_bill_amt]
+#     features=[C_Limit,C_sex,C_edu,C_marg,C_age,pay1,pay2,pay3,pay4,pay5,pay6,BAmt1,BAmt2,BAmt3,BAmt4,BAmt5,BAmt6,PAmt1,PAmt2,PAmt3,PAmt4,PAmt5,PAmt6,avg_bill_amt]
+    features=[C_Limit,C_age,BAmt1,BAmt2,BAmt3,BAmt4,BAmt5,BAmt6,PAmt1,PAmt2,PAmt3,PAmt4,PAmt5,PAmt6,avg_bill_amt]
     features_s = scaler.transform(np.array(features,ndmin=2))
     pred = model.predict(features_s)
 
